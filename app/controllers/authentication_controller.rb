ad866a6f2c339dee3fd181a28b53687fee5458eb
duongvancuong <vancuong@279dn@gmail.com>
2018-03-13 15:43:12 +0700

    Authetication for User

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,21 +1,19 @@
 class AuthenticationController < ApplicationController
-  # return auth token once user is authenticated
   skip_before_action :authorize_request, only: :authenticate
+
   def authenticate
     auth_token =
-      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).perform!
     hash_authen = {
       status: true,
       data: {
-          token: auth_token,
-          name: "cuongdv"
+          token: auth_token
       }
     }
     json_response(hash_authen)
   end
 
   private
-
   def auth_params
     params.permit(:email, :password)
   end
