93516216808cb3d266aae0073987f9b493235fa8
duongvancuong <vancuong@279dn@gmail.com>
2018-03-14 13:57:44 +0700

    Add action refresh token when token expired

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,20 +1,33 @@
 class AuthenticationController < ApplicationController
-  skip_before_action :authenticate!, only: :login
+  skip_before_action :authenticate!, only: [:login, :refresh_token]
 
   def login
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).perform!
     hash_authen = {
       status: true,
-      data: {
-          token: auth_token
-      }
+      token: auth_token
     }
     json_response(hash_authen)
+  end
+
+  def logout
+    token = AuthToken.find_by token: request.headers['Authorization']
+    token ? token.destroy : raise(ExceptionHandler::AuthenticationError)
+  end
+
+  def refresh_token
+    token = AuthToken.find_by refresh_token: refresh_token_param[:refresh_token]
+    token ? token.renew! : raise(ExceptionHandler::AuthenticationError)
+    json_response(token)
   end
 
   private
   def auth_params
     params.permit(:email, :password)
+  end
+
+  def refresh_token_param
+    params.permit(:refresh_token)
   end
 end
